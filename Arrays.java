bf73c2cd6879a86fea596d0f5a42177db4f43869
Anirudh <anirudh11042005@gmail.com>
2025-09-03 15:59:37 +0530

    This is the file and the code for the array file

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args){
+        //This is the initialization of unidimensional array
+        int [] array = {1,2,3,4,5,6};
+        //Accesing the elements of the array
+        for(int i =0;i<array.length;i++){
+            System.out.println(array[i]);
+
+        }
+
+    }
+    
+}
