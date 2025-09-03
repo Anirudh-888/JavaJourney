8fe8a5dcd9391577ac94a20b4c4e0fa90125fa22
Anirudh <anirudh11042005@gmail.com>
2025-09-03 11:01:17 +0530

    This is the hello world code in java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        //This is the syntax that is used to print anything in java
+        System.out.println("Hello World");
+    }
+}
