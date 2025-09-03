905811a4eeb761b3808c1e4c41514d80a5c5f825
Anirudh <anirudh11042005@gmail.com>
2025-09-03 23:10:04 +0530

    This is the oops concept code in java and the creation of class and creating an object to access the instances of the class

---
+++ OOPSinJava_class.java
@@ -0,0 +1,9 @@
+public class OOPSinJava_class {
+    int a = 10;//creating an instance of the class 
+    public static void main(String[] args){
+        //creating an object of the class 
+        OOPSinJava_class o = new OOPSinJava_class();
+        System.out.println(o.a);
+    }
+    
+}
