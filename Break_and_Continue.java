9eb78368bea4817348e4cf45fc8bf37d15ff6c5f
Anirudh <anirudh11042005@gmail.com>
2025-09-03 21:03:19 +0530

    This is the pracitce code that shows the working of break and continue

---
+++ Break_and_Continue.java
@@ -0,0 +1,19 @@
+public class Break_and_Continue {
+    public static void main(String[] args){
+        for(int i = 0;i<10;i++){
+            if(i==4){
+                break;
+            }
+            //This will not print the value after 3 as we have given if i = 4 break so this will come out of the loop
+            System.out.println(i);
+        }
+        for (int j = 0;j<10;j++){
+            if(j==4){
+                continue;
+            }
+            //this will skip the number 4 and then print till 10
+            System.out.println(j);
+        }
+    }
+    
+}
