5d740739425f91f30b30b8f8cf4dc3116858b278
Anirudh <anirudh11042005@gmail.com>
2025-09-03 20:52:23 +0530

    This is the practice code for ternary operator

---
+++ Ternary_opertor.java
@@ -0,0 +1,11 @@
+public class Ternary_opertor {
+    public static void main(String[] args){
+        int age = 17;
+        //this is the usage of ternary operator
+        //Ternary operator is the short form of if and else statements
+        String vote = (age>=18)?"He can vote":"He cannot vote";
+        //Printing out what the value of the String stores
+        System.out.println(vote);
+    }
+    
+}
