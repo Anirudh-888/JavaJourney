381f844bef2c297db46355a12244208ff2679b83
Anirudh <anirudh11042005@gmail.com>
2025-09-03 20:43:13 +0530

    This is the practice code for multidimensional arary

--- Md_Array.java
+++ Md_Array.java
@@ -1,3 +1,15 @@
 public class Md_Array {
+    public static void main(String[] args){
+    //declaring a multidimensional array
+    int [] [] array = {{1,2,3,4,5,6},{1,2,3,4,5,6}};
+    //This is to traverse the elements of the multidimensional array
+    for(int i = 0; i<array.length;i++){
+        for(int j = 0;j<array[i].length;j++){
+            System.out.println(array[i][j]);
+        }
+    }
+    //This is to access the array elements by the index of the array
+    System.out.println(arrray[0][1]);
+    }
     
 }
