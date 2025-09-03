351aa903c2efe41d051eb0e21cab409343db66e8
Anirudh <anirudh11042005@gmail.com>
2025-09-03 11:07:39 +0530

    Adding the code for initialising and printing the value of variables

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args){
+        //These are the variables that are declared
+        int a = 10;
+        double d = 3.1452;
+        char c = 'A';
+        String name = "Anirudh";
+        boolean bool = true;
+        //And the variables that are declared are printed here
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(name);
+        System.out.println(bool);
+
+
+    }
+    
+}
